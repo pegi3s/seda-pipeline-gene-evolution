0e2f99fa106f76d00016fd4a0b64d025bdf38038
Hugo López-Fernández <hlfernandez@uvigo.es>
2025-09-18 20:13:42 +0200

    Removes unnecessary folders

--- task-scripts/cleanup.sh
+++ task-scripts/cleanup.sh
@@ -2,7 +2,7 @@
 
 echo "Performing cleanup"
 
-rm -rf ${workingDirectory}/output ${workingDirectory}/input/getorf ${workingDirectory}/input/replace_1 ${workingDirectory}/input/replace_2 ${workingDirectory}/input/prosplign-procompart ${workingDirectory}/input/reformat_2
+rm -rf ${workingDirectory}/output
 
 # Make sure all results are accessible
 chmod -R 777 "${workingDirectory}/results"
--- task-scripts/replace_2.sh
+++ task-scripts/replace_2.sh
@@ -1,14 +1,11 @@
+#!/bin/bash
+
 # Replace spaces by underscores
 
-mkdir -p ${workingDirectory}/output/replace_2 ${workingDirectory}/input/replace_2 >/dev/null 2>&1
+mkdir -p ${workingDirectory}/output/replace_2 >/dev/null 2>&1
 cp ${workingDirectory}/output/remove-redundant_2/* ${workingDirectory}/output/replace_2 >/dev/null 2>&1
 sed -i 's/_/ /g' ${workingDirectory}/output/replace_2/* >/dev/null 2>&1
 sed -i 's/GCA /_GCA_/g' ${workingDirectory}/output/replace_2/* >/dev/null 2>&1
 sed -i 's/GCF /_GCF_/g' ${workingDirectory}/output/replace_2/* >/dev/null 2>&1
 sed -i -E 's/\.([0-9]+) /_\1_/g' ${workingDirectory}/output/replace_2/* >/dev/null 2>&1
 sed -i -E 's/ \(from ([0-9]+) ([0-9]+)\) /_\(\1-\2\)_/g' ${workingDirectory}/output/replace_2/* >/dev/null 2>&1
-
-# Copying files to the right places
-
-cp ${workingDirectory}/output/replace_2/* ${workingDirectory}/input/replace_2 >/dev/null 2>&1
-chmod -R 777 ${workingDirectory}/input >/dev/null 2>&1
