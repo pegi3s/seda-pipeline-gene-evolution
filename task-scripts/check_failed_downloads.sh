461e1d2fe61df305412f782fdec3890cdfe1bf92
Hugo López Fernández <hlfernandez@uvigo.es>
2025-10-02 17:26:20 +0200

    Adds check failed downloads task

---
+++ task-scripts/check_failed_downloads.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f "${workingDirectory}/output/download-genomes/failed_downloads.txt" ]; then
+    if [ -s "${workingDirectory}/output/download-genomes/failed_downloads.txt" ]; then
+        echo "The following accessions failed to download in the download_genomes task:"
+        cat "${workingDirectory}/output/download-genomes/failed_downloads.txt"
+        echo "Please check the error logs for more details."
+        exit 1
+    else
+        echo "No failed downloads detected."
+    fi
+else
+    echo "No failed downloads detected."
+fi
