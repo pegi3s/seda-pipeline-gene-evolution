61e0333b84e75f98db275faa44c95472a107adf7
Hugo López Fernández <hlfernandez@uvigo.es>
2025-10-04 10:36:59 +0200

    Checks if there are failed accessions before continuing

--- task-scripts/check_failed_downloads.sh
+++ task-scripts/check_failed_downloads.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Function to check for failed accessions in a specific task
+check_failed_accessions() {
+    local task_id="$1"
+    accessions_file="${workingDirectory}/${output}/_failed/${task_id}/failed_accessions.txt"
+
+    if [ -d "${workingDirectory}/${output}/_failed/${task_id}/" ]; then
+        if [ -f ${accessions_file} ]; then
+            if [ -s ${accessions_file} ]; then
+                echo "The following accessions failed in the ${task_id} task:"
+                cat ${accessions_file}
+                echo "Please check the error logs for more details."
+                exit 1
+            fi
+        fi
+    fi
+}
+
 if [ -f "${workingDirectory}/${output}/download-genomes/failed_downloads.txt" ]; then
     if [ -s "${workingDirectory}/${output}/download-genomes/failed_downloads.txt" ]; then
         echo "The following accessions failed to download in the download_genomes task:"
@@ -7,8 +24,12 @@ if [ -f "${workingDirectory}/${output}/download-genomes/failed_downloads.txt" ];
         echo "Please check the error logs for more details."
         exit 1
     else
-        echo "No failed downloads detected."
+        echo "No failed downloads detected in the download_genomes task."
     fi
 else
-    echo "No failed downloads detected."
+    echo "No failed downloads detected in the download_genomes task."
 fi
+
+check_failed_accessions "getorf_and_blast"
+check_failed_accessions "prosplign_procompart"
+echo "No failed downloads or processing detected."
